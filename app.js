d15e24f5baa4db97616b096fa8b54515e3f7bb23
18eddymedina <18emedina@gmail.com>
2018-11-03 13:42:55 -0500

    fixed js

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 var term=document.getElementById("term").value;
 var number=document.getElementById("number").value;
 number=Number(number);
-var startYear=document.getElementById("Syear").value;
-var endYear=document.getElementById("Eyear").value;
+var startYear=document.getElementById("yearS").value;
+var endYear=document.getElementById("yearE").value;
 
+
+startYear+="0101";
+endYear+="1231";
 if(startYear===""){
 var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
 url += '?' + $.param({
+    
   'api-key': "aa93bd10e08449e8973f2634d823eb1c",
-  'begin_date': startYear+"0101",
-  'end_date': endYear+"1231",
+  'q': "dog",
+  'begin_date':startYear,
+  'end_date': endYear,
 });
 $.ajax({
   url: url,
@@ -17,13 +22,8 @@ $.ajax({
 }).done(function(result) {
   console.log(result);
 
-//whatever number is, loop that many times and print out 
 
-
-
-}).fail(function(err) {
-  throw err;
-});
+})
 }
 
 
